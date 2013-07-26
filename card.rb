7d5b0bfdc5034594c8b01cb11d0c91e9dba9c695
Apprentice <apprentice@dbc07.local>
2013-07-26 11:12:10 -0700

    Written class card

---
+++ card.rb
@@ -0,0 +1,20 @@
+class Card
+
+  attr_reader :status, :definition, :answer
+    
+  def initialize(args)
+    @definition = args[:definition]
+    @answer = args[:answer]
+    @status = args.fetch(:status){ :unanwserd }
+  end
+
+  def answered?
+    status == :answered
+  end
+
+  def mark!
+    @status = :answered
+  end
+
+end
+
